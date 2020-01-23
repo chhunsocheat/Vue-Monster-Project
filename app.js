c1c84ce22692db34af04a40e7921528f8bfe232c
Socheat Chhun <chhunsocheat@gmail.com>
2020-01-23 14:58:34 +1100

    new monster health

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ new Vue({
     el:"#app",
     data:{
         playerHealth:100,
-        monsterHealth:100
+        monsterHealth:100,
+        gameIsRunning:false
+    },
+    methods:{
+        startGame: function(){
+            this.gameIsRunning=true;
+            this.playerHealth=100;
+            this.monsterHealth=100;
+        }
     }
 })
