99cbf94c58ec6afb65f0e4e1982ac58359e31863
Socheat Chhun <chhunsocheat@gmail.com>
2020-01-29 00:18:47 +1100

    new button

--- app.js
+++ app.js
@@ -12,14 +12,9 @@ new Vue({
             this.monsterHealth=100;
         },
         attack:function(){
-            let max=10;
-            let min=3;
-            let damage=Math.max(Math.floor(Math.random()*max)+1,min)
-            this.playerHealth-=damage;
-             max=15;
-             min=3;
-            damage=Math.max(Math.floor(Math.random()*max)+1,min)
-            this.monsterHealth-=damage;
+            
+            this.playerHealth-=this.calculateDamage(3,15);
+            this.monsterHealth-=this.calculateDamage(3,20);
             if(this.playerHealth<=0){
                 alert("You've Lost")
                 this.startGame();
@@ -33,12 +28,21 @@ new Vue({
             let min=3;
             let damage=Math.max(Math.floor(Math.random()*max)+1,min)
             this.playerHealth-=damage;
+            this.monsterHealth-=damage;
         },
         heal:function(){
-
+            let max=20;
+            let min=3;
+            let damage=Math.max(Math.floor(Math.random()*max)+1,min)
+            this.playerHealth+=damage;
         },
         giveUp:function(){
-
+            alert("You are a loser");
+            this.gameIsRunning=false;
+            this.startGame();
         },
+        calculateDamage:function(min,max){
+            return Math.max(Math.floor(Math.random()*max)+1,min)
+        }
     }
 })
